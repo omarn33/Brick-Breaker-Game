d6ae70fc88c986d23bb8e49081321e3a29300f8a
omarn33 <omarnaeem333@gmail.com>
2020-12-09 04:05:50 -0600

    Implement Paddle maneuvering methods

--- include/core/paddle.h
+++ include/core/paddle.h
@@ -11,25 +11,29 @@ namespace brickbreaker {
         /** Paddle Attributes */
         glm::vec2 paddle_top_left_corner_;
         glm::vec2 paddle_bottom_right_corner_;
+        float paddle_sensitivity_;
 
         /** Container Attributes */
         glm::vec2 container_top_left_corner_;
         glm::vec2 container_bottom_right_corner_;
+        float container_stroke_width_;
 
         friend class Ball;
 
-        friend class Level;
-
     public:
 
-        Paddle(const glm::vec2 paddle_top_left_corner, const glm::vec2 &paddle_bottom_right_corner,
-               const glm::vec2 &container_top_left_corner, const glm::vec2 &container_bottom_right_corner);
+        Paddle(const glm::vec2 paddle_top_left_corner, const glm::vec2 &paddle_bottom_right_corner, float paddle_sensitivity,
+               const glm::vec2 &container_top_left_corner, const glm::vec2 &container_bottom_right_corner, float container_stroke_width);
 
         /** Getter Methods */
         const glm::vec2 &GetPaddlePosition();
 
         /** Setter Methods */
         void SetPaddlePosition(const glm::vec2 &position);
+
+        void MovePaddleRight();
+
+        void MovePaddleLeft();
 
         /**
          * Draws paddle in the cinder application
--- src/core/paddle.cpp
+++ src/core/paddle.cpp
@@ -3,15 +3,45 @@
 namespace brickbreaker {
 
     Paddle::Paddle(const glm::vec2 paddle_top_left_corner, const glm::vec2 &paddle_bottom_right_corner,
-                   const glm::vec2 &container_top_left_corner, const glm::vec2 &container_bottom_right_corner) {
+                   float paddle_sensitivity, const glm::vec2 &container_top_left_corner,
+                   const glm::vec2 &container_bottom_right_corner, float container_stroke_width) {
         paddle_top_left_corner_ = paddle_top_left_corner;
         paddle_bottom_right_corner_ = paddle_bottom_right_corner;
+        paddle_sensitivity_ = paddle_sensitivity;
         container_top_left_corner_ = container_top_left_corner;
         container_bottom_right_corner_ = container_bottom_right_corner;
+        container_stroke_width_ = container_stroke_width;
     }
 
+    const glm::vec2 &Paddle::GetPaddlePosition() {
+        return paddle_top_left_corner_;
+    }
 
+    void Paddle::SetPaddlePosition(const glm::vec2 &position) {
+        paddle_top_left_corner_ = position;
+    }
 
+    void Paddle::MovePaddleRight() {
+        if(paddle_bottom_right_corner_.x + (container_stroke_width_ / 2) + paddle_sensitivity_ <= container_bottom_right_corner_.x) {
+            paddle_top_left_corner_.x += paddle_sensitivity_;
+            paddle_bottom_right_corner_.x += paddle_sensitivity_;
+        }
+    }
+
+    void Paddle::MovePaddleLeft() {
+        if(paddle_top_left_corner_.x - (container_stroke_width_ / 2) - paddle_sensitivity_ <= container_top_left_corner_.x) {
+            paddle_top_left_corner_.x -= paddle_sensitivity_;
+            paddle_bottom_right_corner_.x -= paddle_sensitivity_;
+        }
+    }
+
+    void Paddle::Draw() {
+
+        ci::gl::color(ci::Color8u(255, 255, 255));
+        ci::gl::Texture2dRef paddle = ci::gl::Texture::create(ci::loadImage(
+                "C:\\Users\\Omar\\Desktop\\Paddle.png"));
+        ci::gl::draw(paddle, ci::Rectf(paddle_top_left_corner_, paddle_bottom_right_corner_));
+    }
 
 } // namespace brickbreaker
 
